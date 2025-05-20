93a935f0717730ec0b4354d1e11333c759f22051
Bugrahan Imal <bugrahanimal@outlook.com>
2025-05-20 20:45:09 +0300

    Improved Uploading

--- scripts.py
+++ scripts.py
@@ -1,5 +1,5 @@
 import logging
-from datasets import load_from_disk
+from datasets import load_from_disk, DatasetDict
 
 from data_load import DataLoader
 
@@ -34,24 +34,39 @@ def load_opp115_data(base_path:str, save_path:str, include_metadata:bool) -> boo
     logging.info("load_opp115_data: Succeed, returning True")
     return True
 
-def upload_opp115_dataset(dataset_path:str, repo_id:str, hf_token:str, commit_message:str, is_private:bool):
+def upload_opp115_dataset(dataset_path:str, repo_id:str, hf_token:str, commit_message:str, is_private:bool) -> None:
     dataset = load_from_disk(dataset_path)
+    if isinstance(dataset, DatasetDict):
+        return 
+    
+    train_testvalid = dataset.train_test_split(test_size=0.2, seed=31)
+    test_validation = train_testvalid["test"].train_test_split(test_size=0.5, seed=31)
 
-    dataset.push_to_hub(
+
+    dataset_dict = DatasetDict({
+        'train': train_testvalid["train"],
+        'test': test_validation["train"],
+        'validation': test_validation["test"]
+    })
+    
+    
+    print(f"Wrapped dataset in DatasetDict: {dataset_dict}")  
+    dataset_dict.push_to_hub(
         repo_id = repo_id,
         token=hf_token,
         commit_message=commit_message,
         private=is_private,
     )
+    print(f"Dataset pushed to Hub repository: {repo_id}")
 
-def handle_load_opp115():
+def handle_load_opp115() -> None:
     base_path = input("BasePath:")
     save_path = input("SavePath:")
     include_metadata = input("IncludeMetadata(Y/N):").lower()
 
     load_opp115_data(base_path, save_path, include_metadata == "y")
 
-def handle_upload_opp115():
+def handle_upload_opp115() -> None:
     dataset_path = input("DatasetPath:")
     repo_id = input("RepoID:")
     token = input("Token:")
